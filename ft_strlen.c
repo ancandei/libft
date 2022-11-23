80fa45ed3419300f9c7e23863682b5da1f5ef126
ancandei <116818643+ancandei@users.noreply.github.com>
2022-11-23 09:14:21 +0000

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -9,8 +9,16 @@
 /*   Updated: 2022/11/11 16:16:48 by ancandei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 size_t  ft_strlen(char *str)
 {
-    fgnbeusnbetyob
+    size_t i;
+
+    i = 0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
 }
